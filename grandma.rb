06941acc4f86ab2db8cd0ec461bb96c942846b8f
Bradley Rush <bradleyj.rush@gmail.com>
2020-03-26 18:14:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(str)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif str.upcase != true
     return "HUH?! SPEAK UP, SONNY!"
-  elsif str == str.upcase
+  elsif str.upcase == true
     return "NO, NOT SINCE 1938!"
   else
     return "Null"
