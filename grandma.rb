83134e2123ddab1ebc4e06028194d33467133efd
Bradley Rush <bradleyj.rush@gmail.com>
2020-03-26 18:18:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,16 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(str)
-  if str == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif str.upcase != true
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif str.upcase == true
-    return "NO, NOT SINCE 1938!"
-  else
-    return "Null"
-  end
-end
+
 
   
